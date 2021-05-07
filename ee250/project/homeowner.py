9cfeeec7b10e9b1b654b9b387ddb93e408db3b5b
Tim Bryer-Ash <bryerash@usc.edu>
2021-05-07 12:23:44 -0700

    refresh

--- ee250/project/homeowner.py
+++ ee250/project/homeowner.py
@@ -27,33 +27,34 @@ def on_message(client, userdata, msg):
 
 def door_status_callback(client, userdata, msg):
     door_status = str(msg.payload, "utf-8")
+    time.sleep(1)
     if lock_status is 0:
     	if door_status == "Motion Detected":
     		print(door_status)
     		with lock:
     			grove_rgb_lcd.setRGB(255,255,255)
-    			grove_rgb_lcd.setText_norefresh("Motion Detected")
+    			grove_rgb_lcd.setText("Motion Detected ")
     		with lock:
     			grovepi.digitalWrite(buzzer, 0)
     	elif door_status == "SAFETY MODE":
     		print(door_status)
     		with lock:
     			grove_rgb_lcd.setRGB(255,0,0)
-    			grove_rgb_lcd.setText_norefresh("SAFETY MODE    ")
+    			grove_rgb_lcd.setText("SAFETY MODE     ")
     		with lock:
     			grovepi.digitalWrite(buzzer, 1)
     	else:
     		print(door_status)
     		with lock:
     			grove_rgb_lcd.setRGB(255,255,255)
-    			grove_rgb_lcd.setText_norefresh("No Motion      ")
+    			grove_rgb_lcd.setText("No Motion       ")
     		with lock:
     			grovepi.digitalWrite(buzzer, 0)
 
 def doorbell_callback(client, userdata, msg):
     with lock:
     		grove_rgb_lcd.setRGB(0,0,255)
-    		grove_rgb_lcd.setText_norefresh("Doorbell Rung   ")
+    		grove_rgb_lcd.setText("Doorbell Rung   ")
     		grovepi.digitalWrite(buzzer, 1)
     		time.sleep(0.1)
     		grovepi.digitalWrite(buzzer, 0)
